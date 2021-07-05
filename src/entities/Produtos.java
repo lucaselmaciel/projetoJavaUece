d193fdbc7a9f54ae0d6fe3f5481bfe28c7331048
lucaselmaciel <lucasmaciel63@gmail.com>
2021-07-05 19:54:28 -0300

    Primeiro commit

---
+++ src/entities/Produtos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+import java.util.Date;
+//import entities.Fornecedores;
+
+/**
+ *
+ * @author lucasmaciel63
+ */
+public class Produtos extends Fornecedores {
+    
+    private String nome;
+    private String fabricante;
+    private Date validade;
+    private Integer codigo_de_barras;
+    
+}
